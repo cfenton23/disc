4edcfe51acd288c53eaf60bb249903f73fb08288
cfenton23 <cfenton23@gmail.com>
2025-08-16 16:52:27 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: '.',
+  publicDir: 'public',
   server: {
-    open: true // This will open your browser when you start the dev server
+    port: 3000,
+    open: true,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': new URL('./system', import.meta.url).pathname
+    }
   }
-});
+})
